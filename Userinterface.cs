78decacea4e4b38a6a658ab6e5d57253d8da26cc
Eva <evaf@itu.dk>
2023-09-12 13:10:18 +0200

    Userinterface refactoring
    Co-authored-by: Patrick patj@itu.dk
    Co-authored-by: Rebekka rebp@itu.dk
    
    new interface-class to handle printing of cheeps

---
+++ Userinterface.cs
@@ -0,0 +1,16 @@
+public static class Userinterface{
+    
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps){
+        foreach (Cheep cheep in cheeps) {
+        Console.WriteLine($"{cheep.Author} @ {TimestampToTime(cheep.Timestamp)}: {cheep.Message}");
+        }
+    }
+
+    private static string TimestampToTime(long timestamp) {
+        return new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(timestamp).ToLocalTime().ToString();
+        }
+}
+
+
+
+
