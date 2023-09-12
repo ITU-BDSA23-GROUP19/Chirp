b63cf3c07dedf787a29f33c25f4568c849f89388
Annabell Philip Nørdam <apno@itu.dk>
2023-09-12 16:44:27 +0200

    Easier parsing of arguments.
    Co-Authored By: Patrick <patj@itu.dk>
    Co-Authored By: Simon <besr@itu.dk>
    Co-Authored By: Rebekka <rebp@itu.dk>
    Co-Authored By: Eva <evaf@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,18 +1,25 @@
 ﻿using SimpleDB;
+using DocoptNet;
+
+const string usage = @"Chirp CLI.
+
+Usage:
+  chirp read <limit>
+  chirp cheep <message>
+  chirp (-h | --help)
+
+";
 
 IDatabaseRepository<Cheep> csvDb = new CSVDatabase<Cheep>("chirp_cli_db.csv");
 
-if (args.Length == 0) {
-    Console.WriteLine("No command used: Use 'dotnet run -- help' to see commands.");
-    return;
-}
+var arguments = new Docopt().Apply(usage, args, exit: true)!;
 
-if (args[0] == "read") {
-    var records = csvDb.Read();
-    Userinterface.PrintCheeps(records);
-} else if (args[0] == "cheep") {
+if(arguments["cheep"].IsTrue) {
     Cheep record = new Cheep(Environment.UserName, args[1], DateTimeOffset.Now.ToUnixTimeSeconds());
     csvDb.Store(record);
+} else if(arguments["read"].IsTrue) {
+    var records = csvDb.Read();
+    Userinterface.PrintCheeps(records);
 }
 
 public record Cheep(string Author, string Message, long Timestamp);
