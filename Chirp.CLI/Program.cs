540162627d5e70f18e41ce868163f7f94647c4c3
Patrick Handberg Jessen <patj@itu.dk>
2023-09-13 19:04:41 +0200

    Updated how docopt reads
    This is done to make it possible to choose amount of cheeps read.

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -6,9 +6,13 @@ IDatabase<Cheep> database = new CSVDatabase<Cheep>("../Resources/database.csv");
 const string usage = @"Chirp commands
 
 Usage:
-  chirp read <limit>
-  chirp cheep <message>
+  chirp read [--limit]
+  chirp cheep (<message>)
   chirp (-h | -help)
+
+Options:
+  -h --help     Show this screen. 
+  --limit       Cheeps shown [default: 5]
 ";
 
 var arguments = new Docopt().Apply(usage, args, exit: true)!;
@@ -16,5 +20,9 @@ var arguments = new Docopt().Apply(usage, args, exit: true)!;
 if (arguments["cheep"].IsTrue) {
     database.Store(new Cheep(arguments["<message>"].ToString()));
 } else if(arguments["read"].IsTrue) {
-    Userinterface.PrintCheeps(database.Read());
+    if (arguments["--limit"].IsNullOrEmpty) {
+        Userinterface.PrintCheeps(database.Read());
+    } else {
+        Userinterface.PrintCheeps(database.Read(arguments["--limit"].AsInt));
+    }
 }
