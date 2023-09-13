0ae3d13946504a0e136b0ce6ba33702af3be8495
Patrick Handberg Jessen <patj@itu.dk>
2023-09-13 16:50:07 +0200

    Updated the docopt commands
    Added a help command.
    This is done such that it is easier too figure out which command to use to run the program.

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -3,11 +3,12 @@ using DocoptNet;
 
 IDatabase<Cheep> database = new CSVDatabase<Cheep>("database.csv");
 
-const string usage = @"Chirp
+const string usage = @"Chirp commands
 
 Usage:
   chirp read <limit>
   chirp cheep <message>
+  chirp (-h | -help)
 ";
 
 var arguments = new Docopt().Apply(usage, args, exit: true)!;
