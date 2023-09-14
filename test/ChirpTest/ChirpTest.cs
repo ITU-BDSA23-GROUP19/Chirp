094f50d23f3964890aa820740541e05dd18ac1a4
Eva <evaf@itu.dk>
2023-09-14 16:31:12 +0200

    first test, forgot references
    co-authored-by Annabell apno@itu.dk

--- test/ChirpTest/ChirpTest.cs
+++ test/ChirpTest/ChirpTest.cs
@@ -3,8 +3,15 @@ namespace test;
 public class ChirpTest
 {
     [Fact]
-    public void Test1()
-    {
+    public void timestampToTimeTest(){
+        //arrange
+        var result = TimestampToDateTime(1690891760);
+
+        //act
+
+        //assert
+        Assert.Equals(result, "08/01/2023 14:09:20")
+
 
     }
 }
