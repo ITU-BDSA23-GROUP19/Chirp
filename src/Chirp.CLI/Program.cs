365486712314c93806a54f5acf8e54356d5dc8c1
Patrick Handberg Jessen <patj@itu.dk>
2023-09-14 14:50:30 +0200

    Updated program.cs to work with the new structure

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,7 +1,7 @@
 ﻿using SimpleDB;
 using DocoptNet;
 
-IDatabase<Cheep> database = new CSVDatabase<Cheep>("../data/database.csv");
+IDatabase<Cheep> database = new CSVDatabase<Cheep>("../../data/database.csv");
 
 const string usage = @"Chirp
 
