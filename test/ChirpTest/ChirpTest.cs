10e18198bbcef2de19ef87c15ed206e01975ed7c
Eva <evaf@itu.dk>
2023-09-16 15:32:11 +0200

    basic tests for cheeps
    not sure if theyre implemeted correctly
    i get an error that something is wrong with the sln file that i cant fix

--- test/ChirpTest/ChirpTest.cs
+++ test/ChirpTest/ChirpTest.cs
@@ -5,13 +5,43 @@ public class ChirpTest
     [Fact]
     public void timestampToTimeTest(){
         //arrange
-        var result = TimestampToDateTime(1690891760);
+        
 
         //act
+        var result = TimestampToDateTime(1690891760);
 
         //assert
-        Assert.Equals(result, "08/01/2023 14:09:20")
+        Assert.Equals(result, "08/01/2023 14:09:20");
 
 
     }
+
+    [Fact]
+    public void CheepGettersTest(){
+//should there be different tests using the two different constructors for cheep??
+        //arrange
+        var cheep = Cheep("ThisAuthor", "A message here", 1690891760);
+
+        //act
+        var authorResult = cheep.Author();
+        var messageResult = cheep.Message();
+        var timestampResult = cheep.Timestamp();
+
+        //assert
+        Assert.Equals(authorResult, "ThisAuthor");
+        Assert.Equals(messageResult, "A message here");
+        Assert.Equals(timestampResult, 1690891760);
+    }
+
+    [Fact]
+    public void CheepDisplayTest(){
+        //arrange
+        var cheep = Cheep("ThisAuthor", "A message here", 1690891760);
+        //act
+        var result = cheep.Display();
+        //assert
+        Assert.Equals(result, "ThisAuthor @ 08/01/2023 14:09:20: A message here");
+    }
+
+
 }
