458ee35451f06c417a9b9360a0e9efea6b551f8f
Annabell Philip Nørdam <apno@itu.dk>
2023-09-19 14:22:29 +0200

    To unit test CSVDatabase class, and test components of the Chirp program
    Co-Authored By: Eva <evaf@itu.dk>

--- test/CSVTest/CSVTest.cs
+++ test/CSVTest/CSVTest.cs
@@ -1,10 +1,57 @@
+using System.ComponentModel.Design;
+
+using SimpleDB;
+
 namespace CSVTest;
 
 public class CSVTest
 {
-    [Fact]
-    public void Test1()
+    //Unit Tests
+    public void CSVDatabase_InstanceExists()
     {
+        //Arrange
+        var database = CSVDatabase<Type>.GetInstance();
+        //Act
 
+        //Assert
+        Assert.NotNull(database);
+    }
+
+    public void Read_Returns_Recrods()
+    {
+        //Arrange
+        var cheep = "This is a randomized tweet!:D";
+
+        //Act
+        var readResult = cheep.Read();
+        //Assert
+        Assert.Contains(cheep, readResult);
+    }
+
+    public void Store_Returns_Equal_Record()
+    {
+        //Arrange
+        var cheep = "This is another randomized cheep, u nuget.";
+        //Act
+        var storeResult = Store(cheep);
+        //Assert
+    }
+
+
+    //Integration Tests
+    [Fact]
+    public void CSVDataLibrary_StorageTest()
+    //M
+    {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    public void CSVDataLibrary_CheepTest()
+    {
+        //Arrange
+        //Act
+        //Assert
     }
 }
