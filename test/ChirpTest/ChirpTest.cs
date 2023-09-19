d6c3355f75effa701ae78173a01a4f32a808eb39
Eva <evaf@itu.dk>
2023-09-19 12:54:40 +0200

    solution file updated
    tests run and are passed

--- test/ChirpTest/ChirpTest.cs
+++ test/ChirpTest/ChirpTest.cs
@@ -3,44 +3,47 @@ namespace test;
 public class ChirpTest
 {
     [Fact]
-    public void timestampToTimeTest(){
+    public void timestampToTimeTest()
+    {
         //arrange
-        
+
 
         //act
-        var result = TimestampToDateTime(1690891760);
+        var result = Utility.TimestampToDateTime(1690891760);
 
         //assert
-        Assert.Equals(result, "08/01/2023 14:09:20");
+        Assert.Equal("08/01/2023 14:09:20", result.ToString());
 
 
     }
 
     [Fact]
-    public void CheepGettersTest(){
-//should there be different tests using the two different constructors for cheep??
+    public void CheepGettersTest()
+    {
+        //should there be different tests using the two different constructors for cheep??
         //arrange
-        var cheep = Cheep("ThisAuthor", "A message here", 1690891760);
+        var cheep = new Cheep("ThisAuthor", "A message here", 1690891760);
 
         //act
-        var authorResult = cheep.Author();
-        var messageResult = cheep.Message();
-        var timestampResult = cheep.Timestamp();
+        var authorResult = cheep.Author;
+        var messageResult = cheep.Message;
+        var timestampResult = cheep.Timestamp;
 
         //assert
-        Assert.Equals(authorResult, "ThisAuthor");
-        Assert.Equals(messageResult, "A message here");
-        Assert.Equals(timestampResult, 1690891760);
+        Assert.Equal("ThisAuthor", authorResult);
+        Assert.Equal("A message here", messageResult);
+        Assert.Equal(1690891760, timestampResult);
     }
 
     [Fact]
-    public void CheepDisplayTest(){
+    public void CheepDisplayTest()
+    {
         //arrange
-        var cheep = Cheep("ThisAuthor", "A message here", 1690891760);
+        var cheep = new Cheep("ThisAuthor", "A message here", 1690891760);
         //act
         var result = cheep.Display();
         //assert
-        Assert.Equals(result, "ThisAuthor @ 08/01/2023 14:09:20: A message here");
+        Assert.Equal("ThisAuthor @ 08/01/2023 14:09:20: A message here", result);
     }
 
 
