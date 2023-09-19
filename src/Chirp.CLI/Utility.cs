418afcd60db1410ad6286766cfd2cd391538a1a9
Patrick Handberg Jessen <patj@itu.dk>
2023-09-19 16:54:14 +0200

    Update Utility.cs

--- src/Chirp.CLI/Utility.cs
+++ src/Chirp.CLI/Utility.cs
@@ -1,6 +1,6 @@
 public class Utility
 {
-    public static String TimestampToDateTime(long timestamp)
+    public static string TimestampToDateTime(long timestamp)
     {
         var time = DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime.ToLocalTime();
         return time.ToString("MM/dd/yyyy HH:mm:ss");
