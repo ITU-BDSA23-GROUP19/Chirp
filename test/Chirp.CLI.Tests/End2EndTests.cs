5678e651cb16dae247e32b7698b0a38137e64561
Patrick Handberg Jessen <patj@itu.dk>
2023-09-20 18:14:40 +0200

    Added eva's end to end test in the right class
    Co-authored-by: Eva <evaf@itu.dk>

--- test/Chirp.CLI.Tests/End2EndTests.cs
+++ test/Chirp.CLI.Tests/End2EndTests.cs
@@ -1,7 +1,39 @@
+using Chirp.SimpleDB;
+
 namespace Chirp.CLI.Tests
 {
     public class End2EndTests
     {
+        [Fact]
+        public void ReadCheeps() //end to end test
+        {
+            //arrange
+            IDatabase<Cheep> database = CSVDatabase<Cheep>.GetInstance();
 
+            TextWriter originalConsoleOut = Console.Out; //store the original console output
+
+            using StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //act
+            //these could be called as Program.Main with arguments to stimulate a user
+            //but it doesnt work for some reason - my guess is it doesnt parse the arguments correctly
+            //and i cant seem to fix it right now
+
+            //writing a test-cheep
+            database.Store(new Cheep("This is a test"));
+            //reading cheeps
+            Userinterface.PrintCheeps(database.Read());
+
+            Console.SetOut(originalConsoleOut);
+            string capturedOutput = stringWriter.ToString();
+
+            //assert
+            Assert.Contains("Hello, BDSA students!", capturedOutput);
+            Assert.Contains("Welcome to the course!", capturedOutput);
+            Assert.Contains("ropf", capturedOutput);
+            Assert.Contains("rnie", capturedOutput);
+            Assert.Contains("This is a test", capturedOutput);
+        }
     }
 }
