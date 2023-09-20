6f5baa6c439b71c2a7751ecbb9415e33bc8a52f1
Patrick Handberg Jessen <patj@itu.dk>
2023-09-20 16:56:59 +0200

    Fixed timestamp converter to match syntax

--- src/Chirp.CLI/Utility.cs
+++ src/Chirp.CLI/Utility.cs
@@ -1,8 +1,9 @@
+using System.Globalization;
+
 public class Utility
 {
     public static string TimestampToDateTime(long timestamp)
     {
-        var time = DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime.ToLocalTime();
-        return time.ToString("MM/dd/yyyy HH:mm:ss");
+        return DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime.ToLocalTime().ToString(CultureInfo.InvariantCulture);
     }
 }
