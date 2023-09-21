13fa6f26a5c204644d2cafcba9aee4ae76a71083
Simon Rasmussen <besr@itu.dk>
2023-09-21 19:10:43 +0200

    Update Utility.cs

--- src/Chirp.CLI/Utility.cs
+++ src/Chirp.CLI/Utility.cs
@@ -4,8 +4,6 @@ public class Utility
 {
     public static string TimestampToDateTime(long timestamp)
     {
-        var utc = DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime.ToUniversalTime();  
-        var cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");           
-        return TimeZoneInfo.ConvertTimeFromUtc(utc, cet).ToString(CultureInfo.InvariantCulture);
+        return DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime.ToLocalTime().ToString(CultureInfo.InvariantCulture);
     }
 }
