e9845fe2e23d6e66f8d095ca33b5da75cc2476c5
Patrick Handberg Jessen <patj@itu.dk>
2023-09-21 14:51:25 +0200

    Update

--- test/Chirp.SimpleDB.Tests/CSVDatabaseTests.cs
+++ test/Chirp.SimpleDB.Tests/CSVDatabaseTests.cs
@@ -1,7 +1,44 @@
+using Chirp.CLI;
+
 namespace Chirp.SimpleDB.Tests
 {
     public class CSVDatabaseTests
     {
+        [Fact]
+        public void CSVDatabase_InstanceExists()
+        {
+            //Arrange
+            var database = CSVDatabase<Type>.GetInstance();
+            //Act
 
+            //Assert
+            Assert.NotNull(database);
+        }
+
+        [Fact]
+        public void Read_Returns_Records()
+        {
+            //Arrange
+            Cheep cheep = new Cheep("This is a randomized tweet!:D");
+            var database = CSVDatabase<Cheep>.GetInstance();
+            //Act
+            database.Store(cheep);
+            var readResult = database.Read();
+            //Assert
+            Assert.Contains(cheep, readResult);
+
+        }
+
+        [Fact]
+        public void Store_Returns_Equal_Record()
+        {
+            //Arrange
+            Cheep cheep = new Cheep("Author", "This is another randomized cheep, u nuget.", 029394848);
+            var database = CSVDatabase<Cheep>.GetInstance();
+            //Act
+            database.Store(cheep);
+            //Assert
+            Assert.Contains(cheep, database.Read());
+        }
     }
 }
