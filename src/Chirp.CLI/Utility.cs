5ac193436a43cc5e0ab129e3bf21f4e087c0c59d
Annabell Philip Nørdam <apno@itu.dk>
2023-09-28 15:02:09 +0200

    To make Chirp.Razor template
    Co-authored-by: Patrick <patj@itu.dk>

--- src/Chirp.CLI/Utility.cs
+++
@@ -1,9 +0,0 @@
-using System.Globalization;
-
-public class Utility
-{
-    public static string TimestampToDateTime(long timestamp)
-    {
-        return DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime.ToLocalTime().ToString(CultureInfo.InvariantCulture);
-    }
-}
--- src/Chirp.SimpleDB/IDatabase.cs
+++
@@ -1,8 +0,0 @@
-namespace Chirp.SimpleDB
-{
-    public interface IDatabase<T>
-    {
-        public IEnumerable<T> Read(int? limit = null);
-        public void Store(T record);
-    }
-}
