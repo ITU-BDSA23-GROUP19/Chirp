d5b5d573a4cb0fe9ecc8f07c5e0bd831c3f1c601
Patrick Handberg Jessen <patj@itu.dk>
2023-09-29 11:30:49 +0200

    Fixed integration test
    This is done to remove error occurring when running "dotnet test" and the test-workflow.

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,11 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<ICheepService, CheepService>();
 
-
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,3 +22,5 @@ app.UseRouting();
 app.MapRazorPages();
 
 app.Run();
+
+public partial class Program { }
---
+++ test/Chirp.Razor.Tests/IntegrationTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Chirp.Razor.Tests
+{
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _fixture;
+        private readonly HttpClient _client;
+
+        public IntegrationTests(WebApplicationFactory<Program> fixture)
+        {
+            _fixture = fixture;
+            _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+        }
+
+        [Fact]
+        public async void CanSeePublicTimeline()
+        {
+            HttpResponseMessage response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Chirp!", content);
+            Assert.Contains("Public Timeline", content);
+        }
+
+        [Theory]
+        [InlineData("Helge")]
+        [InlineData("Rasmus")]
+        public async void CanSeePrivateTimeline(string author)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"/{author}");
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Chirp!", content);
+            Assert.Contains($"{author}'s Timeline", content);
+        }
+    }
+}
