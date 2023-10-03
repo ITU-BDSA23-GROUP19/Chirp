e4995212cddb0398a5752ae8be64303a3277afc0
Patrick Handberg Jessen <patj@itu.dk>
2023-10-03 15:53:11 +0200

    Added unix time method to DBFacade
    Co-authored-by: Rebekka <rebp@itu.dk>

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,7 +1,5 @@
 using System.Data;
 
-using Microsoft.Data.Sqlite;
-
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
 public interface ICheepService
@@ -20,16 +18,7 @@ public class CheepService : ICheepService
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
     {
-        // filter by the provided author name
         DBFacade facade = new DBFacade();
         return facade.GetAuthorCheeps(author);
-    }
-
-    public static string UnixTimeStampToDateTimeString(double unixTimeStamp)
-    {
-        // Unix timestamp is seconds past epoch
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(unixTimeStamp);
-        return dateTime.ToString("MM/dd/yy H:mm:ss");
     }
 }
--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -9,9 +9,13 @@ public class DBFacade
     public DBFacade()
     {
         string? value = Environment.GetEnvironmentVariable("CHIRPDBPATH");
-        if (value == null) {
+
+        if (value == null)
+        {
             _path = "/tmp/chirp.db";
-        } else {
+        }
+        else
+        {
             _path = value;
         }
     }
@@ -35,28 +39,39 @@ public class DBFacade
     {
         SqliteConnection connection = new SqliteConnection($"Data Source={_path}");
         connection.Open();
+
         return connection;
     }
 
     private List<CheepViewModel> RunQuery(string query)
     {
         SqliteConnection connection = ConnectToDB();
+
         SqliteCommand command = connection.CreateCommand();
         command.CommandText = query;
         SqliteDataReader reader = command.ExecuteReader();
+
         return RetrieveCheeps(reader);
     }
 
-    private List<CheepViewModel> RetrieveCheeps(SqliteDataReader reader) {
+    private List<CheepViewModel> RetrieveCheeps(SqliteDataReader reader)
+    {
         List<CheepViewModel> cheeps = new List<CheepViewModel>();
-        
+
         while (reader.Read())
         {
-            cheeps.Add(new CheepViewModel(reader.GetString(0), 
-                                          reader.GetString(1), 
-                                          CheepService.UnixTimeStampToDateTimeString(reader.GetInt32(2))));
+            cheeps.Add(new CheepViewModel(reader.GetString(0),
+                                          reader.GetString(1),
+                                          UnixTimeStampToDateTimeString(reader.GetInt32(2))));
         }
 
         return cheeps;
+    }
+
+    private string UnixTimeStampToDateTimeString(double unixTimeStamp)
+    {
+        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp);
+        return dateTime.ToString("MM/dd/yy H:mm:ss");
     }
 }
