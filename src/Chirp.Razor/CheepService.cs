04c7c528c7090ddfe4a41d4b6e10cdcc4a2c97ab
Patrick Handberg Jessen <patj@itu.dk>
2023-10-04 14:24:29 +0200

    Update CheepService.cs

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,7 +1,5 @@
 using System.Data;
 
-using Microsoft.Data.Sqlite;
-
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
 public interface ICheepService
@@ -12,7 +10,6 @@ public interface ICheepService
 
 public class CheepService : ICheepService
 {
-    // These would normally be loaded from a database for example
     private static readonly List<CheepViewModel> _cheeps = new()
         {
             new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
@@ -26,13 +23,11 @@ public class CheepService : ICheepService
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
     {
-        // filter by the provided author name
         return _cheeps.Where(x => x.Author == author).ToList();
     }
 
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
     {
-        // Unix timestamp is seconds past epoch
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         dateTime = dateTime.AddSeconds(unixTimeStamp);
         return dateTime.ToString("MM/dd/yy H:mm:ss");
