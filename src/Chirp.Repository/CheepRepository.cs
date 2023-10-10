bd4b4b3e8f33e6c6f52ba24cbe88d7c3ba0461dc
Eva <evaf@itu.dk>
2023-10-10 13:14:48 +0200

    added methods
    Co-authored-by: Annabell <abno@itu.dk>
    Co-authored-by: Patrick <patj@itu.dk>

--- src/Chirp.Repository/CheepRepository.cs
+++ src/Chirp.Repository/CheepRepository.cs
@@ -1,4 +1,13 @@
+
 public class CheepRepository : ICheepReposity
 {
+    public List<CheepViewModel> GetCheeps()
+    {
+        throw new NotImplementedException();
+    }
 
+    public List<CheepViewModel> GetCheepsFromAuthor(string author)
+    {
+        throw new NotImplementedException();
+    }
 }
