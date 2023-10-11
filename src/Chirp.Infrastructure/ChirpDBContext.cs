e2699bdb176117ef448b494d884e343333815bb4
Annabell Philip Nørdam <apno@itu.dk>
2023-10-11 15:37:04 +0200

    To instantiate database in context instead of program.cs

--- src/Chirp.Infrastructure/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/ChirpDBContext.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+
 using Chirp.Infrastructure.Models;
 
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +11,24 @@ public class ChirpDBContext : DbContext
     public DbSet<Cheep>? Cheeps { get; set; }
     public DbSet<Author>? Authors { get; set; }
 
+    public string DbPath { get; }
+
+    public ChirpDBContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "chirp.db");
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
+        string fullPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+        string[] paths = new string[] { fullPath, "src", "Chirp.Infrastructure", "data", "chirp.db" };
+
+        Console.WriteLine("ulul");
+
+        Console.WriteLine(Path.Combine(paths));
+
         options.UseSqlite($"Data Source={Path.Combine(Path.GetTempPath(), "chirp.db")}");
     }
 }
