1ec0fdf5e9bf56b462acaef13835f87e1c0aead7
Patrick Handberg Jessen <patj@itu.dk>
2023-10-11 23:02:34 +0200

    Update ChirpDBContext.cs

--- src/Chirp.Infrastructure/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/ChirpDBContext.cs
@@ -11,26 +11,18 @@ public class ChirpDBContext : DbContext
     public DbSet<Cheep>? Cheeps { get; set; }
     public DbSet<Author>? Authors { get; set; }
 
-    public string DbPath { get; }
-
-    public ChirpDBContext()
-    {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "chirp.db");
-    }
-
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
 
         string filePath = Path.GetFullPath(".");
         string fileName = Path.GetFileName(filePath);
 
-        while(!fileName.Equals("Chirp")){
+        while (!fileName.Equals("Chirp"))
+        {
             filePath = Path.GetDirectoryName(filePath);
             fileName = Path.GetFileName(filePath);
         }
-       
+
         string[] paths = new string[] { filePath, "src", "Chirp.Infrastructure", "data", "chirp.db" };
 
         options.UseSqlite($"Data Source={Path.Combine(paths)}");
