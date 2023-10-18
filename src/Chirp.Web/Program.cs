ed061e638dcada58686bca9f799858a71620a26b
Patrick Handberg Jessen <patj@itu.dk>
2023-10-18 23:24:48 +0200

    Changed Program class to be more clean
    This is done by removing partial class and using a normal class and adding a main method.

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,34 +1,38 @@
-using Chirp.Web;
+namespace Chirp.Web;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ChirpContext>(
-    options => options.UseSqlite(builder.Configuration.GetConnectionString("Chirp")));
-builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-
-WebApplication app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddDbContext<ChirpContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("Chirp")));
+        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
+        WebApplication app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        using (IServiceScope scope = app.Services.CreateScope())
+        {
+            IServiceProvider services = scope.ServiceProvider;
+            ChirpContext context = services.GetRequiredService<ChirpContext>();
+            DbInitializer.SeedDatabase(context);
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-using (IServiceScope scope = app.Services.CreateScope())
-{
-    IServiceProvider services = scope.ServiceProvider;
-    ChirpContext context = services.GetRequiredService<ChirpContext>();
-    DbInitializer.SeedDatabase(context);
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapRazorPages();
-
-app.Run();
-
-public partial class Program { }
