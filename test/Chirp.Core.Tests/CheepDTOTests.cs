400f81d78ef66681d1964a125dfbd888912d921b
Patrick Handberg Jessen <patj@itu.dk>
2023-10-19 12:52:17 +0200

    Added CheepDTO tests

--- test/Chirp.Core.Tests/CheepDTOTests.cs
+++ test/Chirp.Core.Tests/CheepDTOTests.cs
@@ -1,10 +1,26 @@
+using System.Runtime.InteropServices;
+
 namespace Chirp.Core.Tests;
 
 public class CheepDTOTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("Rasmus", "Hej, velkommen til kurset.", "2023-08-01 13:08:28")]
+    [InlineData("Helge", "Hello, BDSA students!", "2023-08-01 12:16:48")]
+    [InlineData("Johnnie Calixto", "What do you think so meanly of him?", "2023-08-01 13:13:56")]
+    public void CanCreateCheepDTO(string author, string text, string timeStamp)
     {
+        // Arrange
+        CheepDTO cheep = new CheepDTO(author, text, timeStamp);
 
+        // Act
+        string Author = cheep.Author;
+        string Text = cheep.Text;
+        string TimeStamp = cheep.TimeStamp;
+
+        // Assert
+        Assert.Equal(author, Author);
+        Assert.Equal(text, Text);
+        Assert.Equal(timeStamp, TimeStamp);
     }
 }
