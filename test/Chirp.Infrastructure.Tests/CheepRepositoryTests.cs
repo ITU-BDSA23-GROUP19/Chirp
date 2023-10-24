838e25b6c7f5070b7f89412891be47486875b8d3
Patrick Handberg Jessen <patj@itu.dk>
2023-10-24 16:34:33 +0200

    Update CheepRepositoryTests.cs
    Co-authored-by: Eva <evaf@itu.dk>
    Co-authored-by: Rebekka <rebp@itu.dk>

--- test/Chirp.Infrastructure.Tests/CheepRepositoryTests.cs
+++ test/Chirp.Infrastructure.Tests/CheepRepositoryTests.cs
@@ -6,12 +6,15 @@ public class CheepRepositoryTests
 
     public CheepRepositoryTests()
     {
-        using SqliteConnection connection = new SqliteConnection("Data Source=:memory:");
+        SqliteConnection connection = new SqliteConnection("Data Source=:memory:");
         connection.Open();
         DbContextOptionsBuilder<ChirpContext> builder = new DbContextOptionsBuilder<ChirpContext>().UseSqlite(connection);
-        using ChirpContext context = new ChirpContext(builder.Options);
+        ChirpContext context = new ChirpContext(builder.Options);
         context.Database.EnsureCreated();
-        context.Authors.Add(new Author(){ Name = "hejsameddejsa", Email = "hejsameddejsa@gmail.com"});
+        context.Authors.Add(new Author() { Name = "hejsameddejsa", Email = "hejsameddejsa@gmail.com" });
+        context.Authors.Add(new Author() { Name = "f1skef1let", Email = "f1skef1let@coldmail.com" });
+        context.Authors.Add(new Author() { Name = "IsbjørnOgSkruetrækker", Email = "isbjørnogskruetrækker@hotmail.com" });
+        context.SaveChanges();
         _repository = new CheepRepository(context);
     }
 
@@ -24,6 +27,8 @@ public class CheepRepositoryTests
 
     [Theory]
     [InlineData("hejsameddejsa", "hejsa med dejsa", "2023-08-01 13:13:23")]
+    [InlineData("f1skef1let", "Jeg elsker fiskefilet.", "2023-08-03 13:13:23")]
+    [InlineData("IsbjørnOgSkruetrækker", "lidt effektikt: lidt godt, meget effektivt: meget godt", "2023-10-01 13:13:23")]
     public async void CanCreateCheep(string author, string text, string timeStamp)
     {
         //Arrange
@@ -34,7 +39,8 @@ public class CheepRepositoryTests
 
         //Assert
         IEnumerable<CheepDTO> cheeeeeeeeeeeeps = await _repository.GetCheepsFromAuthorAsync(author);
-        foreach (CheepDTO cheep1 in cheeeeeeeeeeeeps) {
+        foreach (CheepDTO cheep1 in cheeeeeeeeeeeeps)
+        {
             Assert.Equal(text, cheep1.Text);
         }
     }
