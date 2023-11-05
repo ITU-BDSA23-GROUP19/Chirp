d718556f6c271637dc91274eb9749048539b884f
Patrick Handberg Jessen <patj@itu.dk>
2023-11-05 17:55:04 +0100

    Added constrains testing

--- test/Chirp.Infrastructure.Tests/ChirpContextTests.cs
+++ test/Chirp.Infrastructure.Tests/ChirpContextTests.cs
@@ -18,4 +18,96 @@ public class ChirpContextTests
         // Assert
         Assert.NotNull(_context);
     }
+
+    [Fact]
+    public void CanCreateAuthorWhereNameExists()
+    {
+        // Act and Assert
+        try
+        {
+            _context.Authors.Add(new Author()
+            {
+                Name = "Simon",
+                Email = "unique1@gmail.com",
+                Cheeps = new List<Cheep>()
+            });
+
+            _context.Authors.Add(new Author()
+            {
+                Name = "Simon",
+                Email = "unique2@gmail.com",
+                Cheeps = new List<Cheep>()
+            });
+
+            _context.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            Assert.Equal("An error occurred while saving the entity changes. See the inner exception for details.", e.Message);
+        }
+    }
+
+    [Fact]
+    public void CanCreateAuthorWhereEmailExists()
+    {
+        // Act and Assert
+        try
+        {
+            _context.Authors.Add(new Author()
+            {
+                Name = "Unique1",
+                Email = "besr@itu.dk",
+                Cheeps = new List<Cheep>()
+            });
+
+            _context.Authors.Add(new Author()
+            {
+                Name = "Unique2",
+                Email = "besr@itu.dk",
+                Cheeps = new List<Cheep>()
+            });
+
+            _context.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            Assert.Equal("An error occurred while saving the entity changes. See the inner exception for details.", e.Message);
+        }
+    }
+
+    // Can't get constraint tested properly, im gonna ask TA.
+    /*
+    [Fact]
+    public void CanCreateAuthorWithLongName()
+    {
+        _context.Authors.Add(new Author()
+        {
+            Name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            Email = "besr@gmail.com",
+            Cheeps = new List<Cheep>()
+        });
+
+        _context.SaveChanges();
+    }
+
+    [Fact]
+    public void CanCreateCheepWithLongText()
+    {
+        Author author = new Author()
+        {
+            Name = "Simon",
+            Email = "besr@itu.dk",
+            Cheeps = new List<Cheep>()
+        };
+
+        _context.Cheeps.Add(new Cheep()
+        {
+            Author = author,
+            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            TimeStamp = DateTime.Parse("2023-08-01 13:13:23")
+        });
+
+        _context.SaveChanges();
+    }
+    */
 }
