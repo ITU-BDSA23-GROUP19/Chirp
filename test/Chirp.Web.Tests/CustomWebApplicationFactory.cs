9f5cb511f872f3c0a53d7d8f36a684d01716ebd5
Patrick Handberg Jessen <patj@itu.dk>
2023-11-05 23:04:50 +0100

    Create CustomFactory
    This is done to setup the environment to run the integration test in memory.
    By removing the existing context and connection and creating a new one.

---
+++ test/Chirp.Web.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            ServiceDescriptor? contextDb = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ChirpContext>));
+            ServiceDescriptor? connectionDb = services.SingleOrDefault(d => d.ServiceType == typeof(DbConnection));
+
+            if (contextDb != null)
+            {
+                services.Remove(contextDb);
+            }
+
+            if (connectionDb != null)
+            {
+                services.Remove(connectionDb);
+            }
+
+            services.AddDbContext<ChirpContext>(options =>
+            {
+                options.UseInMemoryDatabase("DataSource=:memory:");
+            });
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                ChirpContext context = serviceProvider.GetRequiredService<ChirpContext>();
+                DbInitializer.SeedDatabase(context);
+            }
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
