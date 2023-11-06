9fb844ac36e69588aa71ad564e34c3b3fcf4294d
Patrick Handberg Jessen <patj@itu.dk>
2023-11-06 20:37:07 +0100

    Added namespace to CustomFactory

--- test/Chirp.Web.Tests/GlobalUsings.cs
+++ test/Chirp.Web.Tests/GlobalUsings.cs
@@ -4,5 +4,4 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc.Testing;
 global using Microsoft.Extensions.DependencyInjection;
-global using Chirp.Web;
 global using Chirp.Infrastructure;
