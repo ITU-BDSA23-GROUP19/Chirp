763bb507e3a46e3e7d6e2650824b492ad9ce3f69
Patrick Handberg Jessen <patj@itu.dk>
2023-11-14 13:51:08 +0100

    Added CheepCount methods to ICheepRepository
    Co-authored-by: Simon <besr@itu.dk>

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -3,6 +3,8 @@ namespace Chirp.Core;
 public interface ICheepRepository
 {
     public void CreateCheep(CheepDTO cheep);
+    public Task<int> GetCheepCountAsync();
+    public Task<int> GetCheepCountFromAuthorAsync(string author);
     public Task<IEnumerable<CheepDTO>> GetCheepsAsync(int pageNumber = 1, int pageSize = 32);
     public Task<IEnumerable<CheepDTO>> GetCheepsFromAuthorAsync(string author, int pageNumber = 1, int pageSize = 32);
 }
