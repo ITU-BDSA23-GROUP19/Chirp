a7cfdd8bf7fef4ff312c01a2d8b45ca3b5b55aa0
Eva <evaf@itu.dk>
2023-11-23 15:35:28 +0100

    followbutton works (sometimes)
    disappears when logging in for some reason???

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -5,4 +5,5 @@ public interface IAuthorRepository
     public void CreateAuthor(AuthorDTO author);
     public Task<AuthorDTO> GetAuthorFromNameAsync(string name);
     public Task<AuthorDTO> GetAuthorFromEmailAsync(string email);
+    public void FollowAuthor(FollowDTO followDTO);
 }
